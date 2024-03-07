fe070e5d9bd1a9552c845c5c81207068aac3214f
Andrei Pirogov Huertas <andrei.pirogov.huertas@gmail.com>
2024-03-07 17:11:35 +0100

    moved css to app.css made scroll-thumb higher

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,5 @@
 import { useRef } from "react";
 import { Navbar } from "../../components/navbar";
-import "./home.css";
 export const Home = () => {
   const aboutRef = useRef<HTMLHeadingElement>(null);
   const portfolioRef = useRef<HTMLHeadingElement>(null);
